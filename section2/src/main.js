0dccef749d97d91b322e8367d6ea577959df908f
Kange Mi Sun <altjsvnf@naver.com>
2025-12-30 14:25:53 +0900

    feat: add camera

--- section2/src/main.js
+++ section2/src/main.js
@@ -12,3 +12,16 @@ const renderer = new THREE.WebGLRenderer({
   canvas,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+const scene = new THREE.Scene();
+// mesh가 near와 far 사이에 있고 시야각(fov) 안에 있어야 카메라에 보인다
+const camera = new THREE.PerspectiveCamera(
+  75, // 시야각
+  window.innerWidth / window.innerHeight, // 종횡비
+  0.1, // 얼마나 가까우면 안보이는지
+  1000 // 얼마나 멀면 안보이는지
+);
+
+// 포지션 설정 안해주면 기본 0,0,0
+camera.position.z = 5;
+scene.add(camera);
