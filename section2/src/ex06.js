8bdf417f820fe597abcf58d7f0a7a2f16fbb19f0
Kange Mi Sun <altjsvnf@naver.com>
2025-12-31 13:34:30 +0900

    feat: 애니메이션 성능 보정

---
+++ section2/src/ex06.js
@@ -0,0 +1,60 @@
+import * as THREE from "three";
+
+export default function example() {
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  camera.position.z = 5;
+  scene.add(camera);
+
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.z = 2;
+  light.position.x = 1;
+  scene.add(light);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({ color: "red" });
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const clock = new THREE.Clock();
+
+  function draw() {
+    // const time = clock.getElapsedTime(); // 시작 후 경과 시간
+    const delta = clock.getDelta(); // 이전 프레임과 현재 프레임의 시간 차이
+    mesh.rotation.y += delta;
+    mesh.position.y += delta;
+    if (mesh.position.y > 3) {
+      mesh.position.y = 0;
+    }
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(draw);
+  }
+
+  const setSize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  };
+
+  window.addEventListener("resize", () => {
+    setSize();
+  });
+
+  draw();
+}
